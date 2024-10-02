eed4603e0294707ebd3b3dd9f1f631b1ff61d2a7
schenok <m.gladyshew@yandex.ru>
2024-10-02 20:24:52 +0300

    feat: connect orders and billing services using rmq

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-order.dto';
 
-@Controller()
+@Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @Post()
+  async createOrder(@Body() request: CreateOrderDto) {
+    return this.ordersService.createOrder(request);
+  }
+
   @Get()
-  getHello(): string {
-    return this.ordersService.getHello();
+  async getOrders() {
+    return this.ordersService.getOrders();
   }
 }
--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { OrdersRepository } from './orders.repository';
+import { BILLING_SERVICE } from './constants/services';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World! (orders app)';
+  constructor(
+    private readonly ordersRepository: OrdersRepository,
+    @Inject(BILLING_SERVICE) private billingClient: ClientProxy,
+  ) {}
+
+  async createOrder(request: CreateOrderDto) {
+    const session = await this.ordersRepository.startTransaction();
+
+    try {
+      const order = await this.ordersRepository.create(request, { session });
+      await lastValueFrom(
+        this.billingClient.emit('order_created', { request }),
+      );
+      await session.commitTransaction();
+
+      return order;
+    } catch (error) {
+      await session.abortTransaction();
+      throw error;
+    }
+  }
+
+  async getOrders() {
+    return this.ordersRepository.find({});
   }
 }
